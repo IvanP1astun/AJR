05560d1748a47a2f832ef5ec20df83d41676ae14
Иван Пластун <super.iip2000@ya.ru>
2026-04-06 13:26:42 +0300

    main

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-token = os.getenv('HH_TOKEN')
+ACCESS_TOKEN = os.getenv('HH_TOKEN')
+RESUME_ID = os.getenv('HH_RESUME_ID')
+USER_EMAIL = os.getenv('MY_EMAIL')
+
+# Настройки заголовков (User-Agent теперь тоже берет email из .env)
+HEADERS = {
+    'User-Agent': f'HH-AutoApply-Bot/1.0 ({USER_EMAIL})',
+    'Authorization': f'Bearer {ACCESS_TOKEN}'
+}
+
+# Остальная логика фильтрации и поиска
+SEARCH_QUERIES = ['Python стажер', 'Python junior', 'Devops инженер']
+STOP_WORDS = ['senior', 'middle', 'lead', 'java', 'c#', 'опыт от 3 лет']
